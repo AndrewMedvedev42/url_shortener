6edf6f810b038734988543eb3cf9128c60a8a4d5
AndrewMedvedev42 <sanjino56@gmail.com>
2024-01-13 16:45:12 +0200

    add title

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,11 +6,12 @@
           <form @submit.prevent="submit(url)">
             <div class="form-group">
               <label for="url">Create short links for easy tracking and sharing</label>
-              <textarea type="url" class="form-control" v-model="url" style="height:150px" />
+              <textarea @focus="isError=false" type="url" class="form-control" v-model="url" style="height:150px" />
             </div>
+            <p class="text-danger" v-show="isError">Sorry, an error occurred while sending the url. Please try again later.</p>
             <div class="for-group" v-show="shortUrl">
               <p>
-                Short URL: :
+                Short URL:
                 <a :href="shortUrl" target="_blank" rel="noopener noreferrer" class="text-primary">{{shortUrl}}</a>
               </p>
             </div>
@@ -31,16 +32,18 @@ export default {
     return {
       url: "",
       shortUrl: "",
+      isError:false
     };
   },
   methods: {
     submit: async function (url) {
       try {
-        const backendOrigin = process.emv.VUE_APP_BACKEND_URL
+        const backendOrigin = process.env.VUE_APP_BACKEND_URL
         const response = await axios.post(`${backendOrigin}/url`, {url});
         this.shortUrl = response.data.shortUrl;
+        this.isError = false;
       } catch (error) {
-        console.log(error);
+        this.isError = true;
       }
     },
   },
